c13fd68343eb428f541c68f97ae0300452cfc980
IvanNunez88 <ivanezf@gmail.com>
2026-03-03 18:34:04 -0600

    first commit

---
+++ SeguimientoTramites/Program.cs
@@ -0,0 +1,41 @@
+using SeguimientoTramites.Common;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Servicios
+builder.Services.AddControllers();
+builder.Services.AddFluentValidationConfig();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Registrar DbContext (conexion a BD)
+builder.Services.AddSingleton<SeguimientoTramites.Data.DbContext>();
+
+// Registrar Services
+builder.Services.AddScoped<SeguimientoTramites.Features.Carreras.CarreraService>();
+builder.Services.AddScoped<SeguimientoTramites.Features.Alumnos.AlumnoService>();
+
+// CORS - para que el FRONT pueda conectarse
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
